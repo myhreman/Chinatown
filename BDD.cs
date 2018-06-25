5816b5a9c0d2211ea42caa9a7918fe5167c89377
Hans Martin Myhre <myhre@hansmartin.no>
2018-06-25 18:15:26 +0200

    Create BDD.cs
    Given, When, Then

---
+++ BDD.cs
@@ -0,0 +1,42 @@
+public abstract class BDD<T> where T : BDD<T>
+    {
+        protected T Given
+        {
+            get
+            {
+                return (T)this;
+            }
+        }
+
+        protected T When
+        {
+            get
+            {
+                return (T)this;
+            }
+        }
+
+        protected T Then
+        {
+            get
+            {
+                return (T)this;
+            }
+        }
+
+        protected T Shall
+        {
+            get
+            {
+                return (T)this;
+            }
+        }
+
+        protected T And
+        {
+            get
+            {
+                return (T)this;
+            }
+        }
+    }
